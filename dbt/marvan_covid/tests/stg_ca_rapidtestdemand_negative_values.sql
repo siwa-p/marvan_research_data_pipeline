882c3aa474bab2d7796eb24cc599a6fe91ecb971
Michael Galo <michael@michaelthinks.co>
2025-07-30 15:43:43 +0000

    added negative value data quality test to ca_rapidtestdemand data

---
+++ dbt/marvan_covid/tests/stg_ca_rapidtestdemand_negative_values.sql
@@ -0,0 +1,25 @@
+{{  config(
+  materialized="test")
+}}
+
+SELECT
+    REF_DATE,
+    DGUID,
+    "North American Industry Classification System (NAICS)",
+    "COVID-19 rapid test kits demand and usage",
+    VECTOR,
+    COORDINATE,
+    VALUE as PERCENT,
+    CASE
+        WHEN STATUS = 'A' THEN 'excellent'
+        WHEN STATUS = 'B' THEN 'very good'
+        WHEN STATUS = 'C' THEN 'good'
+        WHEN STATUS = 'D' THEN 'acceptable'
+        WHEN STATUS = 'E' THEN 'use with caution'
+        WHEN STATUS = 'F' THEN 'too unreliable to be published'
+        WHEN STATUS = '..' THEN 'not available for reference period'
+        ELSE STATUS
+    END AS DATA_QUALITY_RATING
+FROM {{ source("RAW", "CA_RAPIDTESTDEMAND_RAW") }}
+WHERE GEO = 'Canada'
+    AND VALUE < 0
