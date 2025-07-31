08f7d4a64bf61f3a351beab8b15ac0d0a58915c4
Michael Galo <michael@michaelthinks.co>
2025-07-31 14:20:37 +0000

    successfully added serialized ids for all tables

--- src/minio_to_snowflake.py
+++ src/minio_to_snowflake.py
@@ -63,6 +63,8 @@ def minio_raw_data_to_snowflake():
 
                     # Add a surrogate primary key column
                     df.insert(0, "id", range(1, len(df) + 1))
+                    logger.info(f"Added surrogate primary key to DataFrame for {obj.object_name}")
+                    logger.info(f"DataFrame head:\n{df.head()}")
 
                     result = write_pandas(
                         conn,
